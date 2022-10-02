feef5b730bcc7ddc2ca95074d175e20ecae1fb90
qccomputerso <91327374+qccomputerso@users.noreply.github.com>
2022-10-02 14:38:47 +0000

    Deploying to gh-pages from @ qccomputerso/qccomputerso.github.io@468f7055a55e4444694047cefe7b100fbd570123 🚀

--- assets/main.a59c3353.js
+++ assets/main.a59c3353.js
@@ -1,5 +1,5 @@
-import{P as n,c as a,_ as e}from"./App.vue_vue_type_script_setup_true_lang.40b42447.js";n.content.notification={title:"Orientation Day!",content:`The O Day will be held soon! Be sure to come for a program demonstration and interesting talks!
-	We may even have quizzes and give out prizes! Details will be announced soon.`};n.content.cards=[{title:"Welcome to our homepage!",content:`
+import{P as a,c as n,_ as e}from"./App.vue_vue_type_script_setup_true_lang.40b42447.js";a.content.notification={title:"Orientation Day!",content:`The Orientation Day will be held on 10/10 at Room 238! Be sure to come for a program demonstration,
+	games showcase, and interesting talks! Fun is guaranteed for all!`};a.content.cards=[{title:"Welcome to our homepage!",content:`
 Our goal as the Society of Computer Studies is to provide insight about programming and Computer Science
 to our schoolmates, and to help them gain a better understanding of the topics, as well as an understanding
 of the impact of rapid development of IT on our society.
@@ -16,7 +16,13 @@ as well as training for the
 	<button>
 		Events >>
 	</button>
-</a>`},{title:"Events",content:"This year, we will hold a programming course and do nothing else."},{title:"Officials List",content:`
+</a>`},{title:"Events",content:`The Orientation day is coming soon, be sure to give us a visit!!!
+<br>
+<br>
+This year, we will hold a programming course which teaches C++, a strongly typed and extremely quick language
+which is often used as a framework for many problems which require high computational speeds. For those who are
+already experienced in programming, we may hold a mini competition which allows you to demonstrate your programming
+skills!`},{title:"Officials List",content:`
 <b>Chairman:</b>
 <div class="o-info-bicolumn-display">
 	<span>Alex Wu</span>
@@ -27,13 +33,13 @@ as well as training for the
 <div class="o-info-bicolumn-display">
 	<span>Edison Fu</span>
 	<span>4A (10)</span>
-	<span>Chen Kyle</span>
+	<span>Kyle Chen</span>
 	<span>5A (04)</span>
 </div>
 <br>
 <b>Committees:</b>
 <div class="o-info-bicolumn-display">
-	<span>Chan Ching Kiu</span>
+	<span>CK Chan Ching Kiu</span>
 	<span>3A (02)</span>
 	<span>Mike Lam</span>
 	<span>4A (17)</span>
@@ -41,7 +47,7 @@ as well as training for the
 	<span>4A (35)</span>
 	<span>Arthur Pang</span>
 	<span>5A (19)</span>
-	<span>Kevin Li Yue Wang</span>
+	<span>Kevin Li</span>
 	<span>5B (19)</span>
 </div>
 <br>
@@ -49,4 +55,4 @@ as well as training for the
 	<button>
 		Contact Us >>
 	</button>
-</a>`}];a(e).mount("#app");
+</a>`}];n(e).mount("#app");
