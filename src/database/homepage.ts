e36399de84e6cb87aa887610bcb8bacb242f438c
QC Society of Computer Studies <91327374+qccomputerso@users.noreply.github.com>
2022-11-01 18:38:12 +0800

    Remove notification for subcom

--- src/database/homepage.ts
+++ src/database/homepage.ts
@@ -3,14 +3,6 @@ import CommitteeList from "@/assets/committee-list.jpeg";
 import { Page } from ".";
 
 
-Page.content.notification = {
-	title: "Subcom Recruitment!",
-	content: `
-We are currently recruiting subcommittee members for the new school year! Fill in the google form now
-<a href="https://forms.gle/vjk1T9JETDSqgmj48" target="_blank">HERE</a>. For any more information you may
-contact us- Details are at the bottom of the page.`
-};
-
 Page.content.cards = [{
 	title: "The Orientation Day is over...",
 	content: `
