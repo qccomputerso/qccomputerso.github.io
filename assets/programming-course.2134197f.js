ab2d173e9cb1e02c6c517baaf591e9236a62d26a
qccomputerso <91327374+qccomputerso@users.noreply.github.com>
2023-02-21 12:38:10 +0000

    Deploying to gh-pages from @ qccomputerso/qccomputerso.github.io@c6ff2ef0b3681f782cb187a1a07efa1e4711d294 🚀

---
+++ assets/programming-course.2134197f.js
@@ -0,0 +1,11 @@
+import{P as o,c as s,_ as e}from"./App.vue_vue_type_script_setup_true_lang.9787acb1.js";o.content.cards=[{title:"Tools for Programming in C++",content:`
+This slide covers what sorts of tools you can use to program in C++. If you find something else more convenient,
+feel free to use it.
+<br>
+<a href="tools_for_coding_in_cpp.pptx">Download</a>`},{title:"Lesson 1",content:`
+This slide covers basic I/O, variables, and program flow.
+<br>
+<a href="lesson1.pptx">Download</a>`},{title:"Lesson 2",content:`
+This slide covers functions, arrays (Essentially lists), and vectors (Expandable arrays).
+<br>
+<a href="lesson2.pptx">Download</a>`}];s(e).mount("#app");
