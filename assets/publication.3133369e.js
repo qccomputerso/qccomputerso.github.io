1b9217f078593410637c8645b60b2406ad41881d
qccomputerso <91327374+qccomputerso@users.noreply.github.com>
2023-03-30 13:35:37 +0000

    Deploying to gh-pages from @ qccomputerso/qccomputerso.github.io@9e6f9474de8f6299fc72c01280edaf8c2fa23ab1 🚀

---
+++ assets/publication.3133369e.js
@@ -0,0 +1,144 @@
+import{d as r,k as B,a as s,w as F,_ as E,o as a,f as C,r as A,C as l,g as h,t as c,e as t,m as o,u as D,F as b,j as d,P as m,c as g}from"./App.vue_vue_type_script_setup_true_lang.4ee3d21a.js";const f=["src"],p=["innerHTML"],y=r({__name:"Publication",setup(_){const u=B(!1);return(k,i)=>(a(),s(E,null,{default:F(()=>[(a(!0),C(b,null,A(D(n),e=>(a(),s(l,{key:e.title},{header:F(()=>[h(c(e.title)+" ",1),t("div",{class:"c-switch-language",onClick:i[0]||(i[0]=P=>{u.value=!u.value})},[t("span",{class:o({"c-switch-language__button":!0,"c-switch-language__button--inactive":u.value})}," Eng ",2),t("span",{class:o({"c-switch-language__button":!0,"c-switch-language__button--inactive":!u.value})}," \u4E2D ",2)])]),default:F(()=>[t("img",{class:"c-publication-image",src:e.imageSrc,width:"250"},null,8,f),t("span",{innerHTML:u.value?e.chinese:e.english},null,8,p)]),_:2},1024))),128))]),_:1}))}});const w=d(y,[["__scopeId","data-v-69d9c9e4"]]);m.content.cards=[];const n=[],v=""+new URL("dec2022-1st.a6d5a917.png",import.meta.url).href;n.push({title:"December 2022- Famous Computer Scientists",imageSrc:v,chinese:`
+\u8A08\u7B97\u6A5F\u79D1\u5B78\u6709\u4ECA\u65E5\u7684\u767C\u5C55\u548C\u6210\u5C31\uFF0C\u5C11\u4E0D\u4E86\u4E00\u8846\u8A08\u7B97\u6A5F\u79D1\u5B78\u5BB6\u5011\u7684\u8CA2\u737B\uFF0C\u4EE5\u4E0B\u6211\u5C31\u4ECB\u7D39\u5E7E\u4F4D\u7D66\u5927\u5BB6\u8A8D\u8B58\u8A8D\u8B58\u5427\uFF01
+<br><br>
+
+<b>\u4E9E\u502B\xB7\u5716\u9748(Alan Turing, 1912-1954)</b>
+<br><br>
+\u5716\u9748\u88AB\u8B7D\u70BA\u300C\u96FB\u8166\u79D1\u5B78\u8207\u4EBA\u5DE5\u667A\u6167\u4E4B\u7236\u300D\uFF0C\u662F\u6700\u6709\u5F71\u97FF\u529B\u7684\u8A08\u7B97\u6A5F\u79D1\u5B78\u5BB6\u4E4B\u4E00\u3002\u4ED6\u767C\u660E\u4E86\u5716\u9748\u6A5F\uFF0C\u4E00\u53F0\u5C07\u4EBA\u5011\u666E\u901A\u7684\u8A08\u7B97\u884C\u70BA\u62BD\u8C61\u5316\u70BA\u908F\u8F2F\u7B97\u5F0F\u7684\u8A08\u7B97\u6A21\u578B\u3002
+\u5B83\u7684\u7528\u9014\u5728\u65BC\u627E\u51FA\u8A08\u7B97\u6578\u5F0F\u53CA\u57F7\u884C\u6307\u4EE4\u7684\u9650\u5236\uFF0C\u53EF\u4EE5\u8655\u7406\u4E16\u4E0A\u6240\u6709\u5B58\u5728\u7684\u96FB\u8166\u904B\u7B97\uFF0C\u662F\u4E00\u500B\u5178\u578B\u7684\u4E2D\u592E\u8655\u7406\u5668(CPU)\u4F8B\u5B50\u3002
+<br>
+\u65BC\u7B2C\u4E8C\u6B21\u4E16\u754C\u5927\u6230\u4E2D\uFF0C\u5716\u9748\u61C9\u7528\u4ED6\u5275\u9020\u7684\u904B\u7B97\u6A21\u578B\uFF0C\u6210\u529F\u52A9\u82F1\u8ECD\u7834\u89E3\u5FB7\u8ECD\u50B3\u9001\u8A0A\u606F\u6240\u7528\u7684\u5BC6\u78BC\uFF0C\u5354\u52A9\u5C07\u5FB7\u8ECD\u7684\u4FB5\u7565\u66AB\u7DE9\uFF0C
+\u8B8A\u76F8\u4FDD\u4F4F\u4E86\u53D7\u6230\u706B\u6CE2\u53CA\u5730\u5340\u7684\u5E7E\u842C\u689D\u4EBA\u547D\uFF0C\u529F\u5FB7\u7121\u91CF\u3002\u7136\u800C\uFF0C\u56E0\u6027\u53D6\u5411\u7DE3\u6545\uFF0C\u4ED6\u65BC1954\u5E74\u81EA\u6BBA\u8EAB\u4EA1\uFF0C\u4EE542\u6B72\u4E4B\u9F61\uFF0C\u82F1\u5E74\u65E9\u901D\u3002
+<br><br>
+
+<b>\u611B\u9054\xB7\u52D2\u8299\u857E\u7D72(Ada Lovelace, 1815-1852)</b>
+<br><br>
+\u52D2\u8299\u857E\u7D72\u7372\u516C\u8A8D\u70BA\u53F2\u4E0A\u9996\u4F4D\u8A08\u7B97\u6A5F\u79D1\u5B78\u5BB6\u30021842\u5E74\uFF0C\u5979\u767C\u8868\u4E86\u53F2\u4E0A\u7B2C\u4E00\u6BB5\u6F14\u7B97\u6CD5\uFF0C\u4E5F\u5373\u662F\u7B2C\u4E00\u6BB5\u96FB\u8166\u7A0B\u5F0F\u3002\u4F5C\u70BA\u65E9\u671F\u7684\u7A0B\u5F0F\u8A2D\u8A08\u5E2B\uFF0C
+\u5979\u771F\u6B63\u8A8D\u8B58\u5230\u8A08\u7B97\u6A5F\u7684\u6F5B\u80FD\uFF0C\u5C0D\u7528\u65BC\u908F\u8F2F\u8A08\u7B97\uFF0C\u57F7\u884C\u8907\u96DC\u6307\u4EE4\u7684\u73FE\u4EE3\u96FB\u8166\u8CA2\u737B\u6B8A\u5049\u3002
+<br><br>
+
+<b>\u6587\u9813\xB7\u745F\u592B(Vinton Cerf, 1943- ) \u53CA \u7F85\u4F2F\u7279\xB7\u5361\u6069(Robert Kahn, 1938- )</b>
+<br><br>
+1983\u5E741\u67081\u65E5\u662F\u4E92\u806F\u7DB2\u9762\u4E16\u7684\u5927\u65E5\u5B50\uFF0C\u65BC\u6B64\uFF0C\u745F\u592B\u548C\u5361\u6069\u5169\u4F4D\u79D1\u5B78\u5BB6\u5C45\u529F\u81F3\u5049\u3002\u4ED6\u5011\u4E00\u8D77\u767C\u660E\u4E86\u50B3\u8F38\u63A7\u5236\u5354\u5B9A(TCP)\u53CA\u7DB2\u969B\u7DB2\u7D61\u5354\u5B9A(IP)\uFF0C
+\u5176\u4E2DIP\u80FD\u6839\u64DA\u4E0D\u540C\u96FB\u8166\u7684\u4F4D\u5740\u4F86\u9032\u884C\u8A8D\u8B49\u53CA\u50B3\u9001\u8CC7\u6599\uFF0C\u662F\u4E92\u806F\u7DB2\u7684\u95DC\u9375\u7D44\u6210\u90E8\u5206\u3002\u81EA\u6B64\uFF0C\u7DB2\u7D61\u4E16\u754C\u7684\u4EA4\u6D41\u8B8A\u5F97\u66F4\u65B9\u4FBF\uFF0C\u53EF\u9760\uFF0C\u6642\u81F3\u4ECA\u65E5\u4ECD\u5F71\u97FF\u6DF1\u9060\u3002
+<br><br>
+
+<b>\u5409\u591A\xB7\u7BC4\u7F85\u8607\u59C6(Guido van Rossum, 1956- )</b>
+<br><br>
+Python \u662F\u7576\u4E0B\u6700\u5E38\u7528\u7684\u7DE8\u7A0B\u8A9E\u8A00\u4E4B\u4E00\uFF0CDropbox\uFF0CBattlefield 2\u7B49\u8457\u540D\u904A\u6232\u3001\u5DE5\u5177\uFF0C\u751A\u81F3\u4E0D\u5C11\u4EBA\u5DE5\u667A\u80FD(AI)\u80CC\u5F8C\u7684\u7A0B\u5F0F\u7686\u4F7F\u7528Python\u8A9E\u8A00\u7DE8\u5BEB\uFF0C
+\u800C\u9019\u8A9E\u8A00\u7684\u8A2D\u8A08\u8005\u4FBF\u662F\u7BC4\u7F85\u8607\u59C6\u30021989\u7684\u8056\u8A95\u7BC0\uFF0C\u4ED6\u5728\u5BB6\u4E2D\u7121\u6240\u4E8B\u4E8B\uFF0C\u4FBF\u958B\u59CB\u5275\u4F5CPython\u8A9E\u8A00\u89E3\u60B6\uFF0C\u6700\u5F8C\u65BC1991\u5E74\u767C\u8868\u4E860.9.0\u7248\u672C\u3002
+\u622A\u81F32022\u5E741\u670814\u65E5\uFF0CPython\u5DF2\u7136\u767C\u5C55\u81F33.11.0\u7248\u672C\u3002
+<br>
+Python\u8A9E\u8A00\u512A\u9EDE\u5728\u65BC\u8A00\u7C21\u610F\u8CC5\uFF0C\u6BD4\u8D77\u5176\u4ED6\u8A9E\u8A00\uFF0C\u57F7\u884C\u76F8\u540C\u7684\u6307\u4EE4\u6642\u80FD\u7701\u4E0B\u4E0D\u5C11\u4EE3\u78BC\uFF0C\u4E14\u8F03\u5BB9\u6613\u660E\u767D\u3002\u4E0D\u904E\uFF0C\u5B83\u57F7\u884C\u6307\u4EE4\u7684\u901F\u5EA6\u8F03\u6162\uFF0C\u56E0\u6B64\u4E0D\u5E38\u7528\u65BC\u5927\u578B\u5C08\u6848\u3002
+`.replaceAll(`
+`,""),english:`
+Even with the amazing developments of modern computer science, we should not forget the giants of the past 
+who paved the way for such a feat. Let's talk about some of them!
+<br><br>
+
+<b>Alan Turing (1912-1954)</b>
+<br><br>
+Turing has been dubbed "the father of computer science and artificial intelligence", quite rightfully as 
+one of the most influential computer scientists even today. He invented the Turing Machine, a hypothetical 
+abstract model of a CPU which is able to handle any computation action that can exist, whether or not it 
+has been invented. It allows people to figure out what limitations any type of computer might have.
+<br>
+In WWII, Turing used his computational model to solve the "Enigma" code used by Nazi Germany, allowing 
+them to decrypt all messages sent by them. This impeded the progres of the Nazis and saved countless 
+lives. Unfortunately, due to his sexuality, he was driven to suicide by the British government at only 
+the age of 42.
+<br><br>
+
+<b>Ada Lovelace (1815-1852)</b>
+<br><br>
+Ada Lovelace is widely regarded as the world's first computer scientist. In 1842, she published the first 
+known example of an "algorithm", which can be thought of as a computer program. As an early example of a 
+programmer, she realised the potential of calculators beyond solving simple arithmetics, and laid groundwork 
+for far more complex computing today.
+<br><br>
+
+<b>Vinton Cerf (1943- ) and Robert Kahn (1938- )</b>
+<br><br>
+On Jan 1 1983, the world saw the Internet for the first time. Computer Scientists Vinton Cerf and Robert Kahn 
+played a big role in its invention, by developing the Transmission Control Protocol (TCP) and Internet Protocol (IP) 
+which help networks communicate correctly and efficiently. IP is the most well known, acting like your home 
+address but for your computer's position in the Internet instead, and it ensures that data is sent to the 
+correct machines. This made the Internet possible, and most importantly, reliable, and the system is still 
+being used and updated today.
+<br><br>
+
+<b>Guido van Rossum (1956- )</b>
+<br><br>
+Python is an all-pervasive language, being used in different tools and games like Dropbox and Battlefield 2, 
+even acting as the language of choice for communicating with AI or robotics frameworks. Guido van Rossum 
+invented python in the Christmas of 1989 when he was bored at home, and released the major edition Python 
+0.9.0 in 1991. Python is still being updated today, with the latest version being 3.11.0. 
+<br>
+Python is intuitive and accomplishes a lot with few words. It is very declarative in nature, with many 
+libraries helping it. However, since Python is fairly slow during execution, it may not be suitable 
+for handling the backend of computationally heavy tasks. 
+`.replaceAll(`
+`,"")});const G=""+new URL("feb2023-1st.79c06971.png",import.meta.url).href;n.push({title:"Feburary 2023- 5G misconceptions",imageSrc:G,chinese:`
+"5G\u5F71\u97FF\u98DB\u884C\u5B89\u5168\uFF1F\u822A\u7A7A\u516C\u53F8\u8B66\u544A\u300C\u53EA\u5DEE200MHz\u300D\u5F88\u5371\u96AA"
+<br><br>
+"K Kwong\u6307\u624B\u6A5F\u4FE1\u865F\u8F3B\u5C04\u5F37\u7761\u89BA\u61C9\u95DC5G"
+<br><br>
+\u76F8\u4FE1\u9019\u6A23\u7684\u65B0\u805E\uFF0C \u5927\u5BB6\u5DF2\u591A\u6709\u8033\u805E\u3002\u7136\u800C\u9019\u4E9B\u771F\u662F\u662F\u4E8B\u5BE6\u55CE\uFF1F5G \u8A2D\u5099\u6703\u4E0D\u6703\u767C\u51FA\u9AD8\u983B\u8F3B\u5C04\u800C\u81F4\u764C\uFF1F5G\u6703\u5C0E\u81F4\u98DB\u6A5F\u5931\u4E8B\uFF1F\u83AB\u6025\uFF0C\u4EE5\u4E0B\u6211\u7232\u5927\u5BB6\u4E00\u4E00\u8AAC\u4F86\u3002
+<br><br>
+5G\u6280\u8853\u662F\u4EC0\u9EBC\uFF1F\u7528\u8655\u5728\u54EA\u88E1\uFF1F
+<br><br>
+\u5F88\u591A\u4EBA\u5C0D5G \u7684\u5370\u8C61\u5C31\u662F\u8D85\u9AD8\u901F\u7684\u7DB2\u7D61\u3002\u6C92\u932F\uFF0C 5G \u7DB2\u7D61\u7684\u901F\u5EA6\u7684\u78BA\u6BD44G \u5FEB\u7D0420\u500D\uFF0C\u5B83\u662F\u6700\u65B0\u4E00\u4EE3\u79FB\u52D5\u901A\u4FE1\u6280\u8853\uFF0C\u70BA4G\u7CFB\u7EDF\u7684\u6F14\u9032\u3002
+5G\u5FEB\u7684\u539F\u56E0\u662F\u5B83\u6BD4\u4E00\u822C\u96FB\u8A0A\u696D\u73FE\u884C\u4F7F\u7528\u7684\u983B\u8B5C\u9AD8\u51FA\u8A31\u591A (\u6BEB\u7C73\u6CE2\u9891\u6BB5)\u3002\u4F46\u96D6\u7136\u5B83\u6709\u6975\u5FEB\u7684\u50B3\u8F38\u901F\u5EA6\uFF0C\u80FD\u9054\u52304G\u7DB2\u7D61\u7684100\u500D\uFF0C\u800C\u4E14\u6642\u5EF6\u5F88\u4F4E\uFF0C
+\u4F46\u8A0A\u865F\u7E5E\u904E\u969C\u7919\u7269\u7684\u80FD\u529B\u5341\u5206\u6709\u9650\uFF0C\u4E14\u50B3\u9001\u8DDD\u96E2\u5F88\u77ED\uFF0C\u56E0\u6B64\u9700\u8981\u5F88\u591A\u7684\u767C\u5C04\u7AD9\u3002
+\u6B63\u56E0\u5B83\u6975\u5FEB\u7684\u50B3\u8F38\u901F\u5EA6\uFF0C 5G\u5EE3\u6CDB\u7528\u65BC\u65B0\u8208\u79D1\u6280\uFF0C\u4F8B\u5982\u7121\u4EBA\u99D5\u99DB\u6C7D\u8ECA\u3001\u9059\u8DDD\u624B\u8853\u3001\u667A\u80FD\u57CE\u5E02\u3001\u865B\u64EC\u5BE6\u5883\u3001\u7121\u4EBA\u6A5F\u7B49\u7B49\uFF0C\u53EF\u8AAA\u5C0D\u4EBA\u985E\u7684\u79D1\u6280\u767C\u5C55\u6709\u83AB\u5927\u7684\u5E6B\u52A9\u3002
+<br><br>
+5G \u5C0D\u5065\u5EB7\u6709\u5A01\u8105\uFF1F
+<br><br>
+\u4E0A\u6587\u63D0\u53CA\uFF0C 5G\u7684\u983B\u6BB5\u8F03\u9AD8\u3002\u6709\u8B80\u904E\u7269\u7406\u7684\u540C\u5B78\u7ACB\u5373\u5C31\u6703\u60F3\u5230\uFF0C\u9AD8\u983B\u5C04\u7DDA\u6703\u5F71\u97FF\u5065\u5EB7\uFF0C\u751A\u81F3\u81F4\u764C\u3002\u4E8B\u5BE6\u771F\u662F\u9019\u6A23\u55CE\uFF1F\u5176\u5BE65G\u53EA\u5C6C\u65BC\u975E\u6E38\u96E2\u6027\u7684\u96FB\u6CE2
+ (non-ionizing radiation)\uFF0C\u548C\u7D2B\u5916\u7DDA\u3001\u4F3D\u746A\u5C04\u7DDA\u7B49\u4E0D\u540C\u3002\u5B83\u4E0D\u80FD\u628A\u96FB\u5B50\u5F9E\u57FA\u672C\u5206\u5B50\u4E2D\u5206\u96E2\u51FA\u4F86\uFF0C\u80FD\u91CF\u5F88\u4F4E\uFF0C\u56E0\u6B64\u4E0D\u6703\u5C0D\u6211\u5011\u7684DNA\u9020\u6210\u50B7\u5BB3\u3002\u82F1\u570B\u4E5F\u6709\u7814\u7A76\u6307\u51FA\uFF0C\u5982\u679C5G\u53EA\u53EF\u7A7F\u900F\u4EBA\u9AD40.2cm\uFF0C \u56E0\u6B64\uFF0C5G\u6703\u81F4\u764C\u7B49\u8AAA\u6CD5\u53EA\u5C6C\u8B20\u8A00\u3002
+ <br><br>
+5G\u4F7F\u98DB\u6A5F\u5931\u4E8B\uFF1F
+<br><br>
+\u822A\u7A7A\u696D\u6E2C\u91CF\u98DB\u6A5F\u9AD8\u5EA6\u7684\u7121\u7DDA\u96FB\u9AD8\u5EA6\u8868\uFF0C\u90FD\u6703\u4F7F\u75284.2 Ghz\u81F34.4 Ghz\u7684\u983B\u8B5C\u3002\u800C\u4E00\u822C\u570B\u5BB65G\u7684\u983B\u8B5C\u662F3.3GHz\u81F33.8GHz\uFF0C\u548C\u822A\u7A7A\u696D\u4F7F\u7528\u7684\u90FD\u76F8\u5DEE\u4E00\u6BB5
+\u8DDD\u96E2\uFF0C\u56E0\u6B64\u4E0D\u6703\u9020\u6210\u5A01\u8105\u3002 \u73FE\u5728\u7F8E\u570B\u806F\u90A6\u822A\u7A7A\u751A\u81F3\u5DF2\u6279\u51C6\u96FB\u8A0A\u5546Verizon\u53CAAT&T\u5728\u6A5F\u5834\u9644\u8FD1\u555F\u7528\u65B05G\u7DB2\u7D61\u3002
+<br><br>
+\u7E3D\u7684\u4F86\u8AAA\uFF0C5G\u5C0D\u4EBA\u985E\u7684\u5F71\u97FF\u5FAE\u4E4E\u5176\u5FAE\u3002 \u56E0\u6B64\u5927\u5BB6\u4E0D\u7528\u64D4\u5FC3\uFF0C \u53EF\u4EE5\u653E\u5FC3\u4F7F\u75285G\u8A2D\u5099\uFF0C\u4EAB\u53D75G\u5E36\u4F86\u7684\u4FBF\u5229\uFF01
+	
+`.replaceAll(`
+`,""),english:`
+"5G may affect air safety, warns airline company."
+<br><br>
+"K Kwong says that 5G should be turned off while sleeping."
+<br><br>
+You\u2019ve probably seen similar headlines before, but are they true? Will 5G really cause cancer or make planes crash?
+<br><br>
+Actually, what even is 5G used for?
+<br><br>
+Most peoples\u2019 know 5G for its high network speed- 5G is quicker than 4G by a magnitude of about 20 times! 
+It\u2019s the newest standard in communications technology, developed from 4G. 5G is extremely fast because it 
+transmits data using waves of high frequencies, meaning that more information can be fit into a single second. 
+Unfortunately, this also has the caveat of making the transmission distance of 5G much shorter, so more 
+broadcasting stations need to be set up.
+<br>
+Due to its high network speed, 5G has allowed for the development of a lot of new technology, in various areas 
+such as self-driving cars, remote surgery, virtual reality, and drones.
+<br><br>
+5G affects health?
+<br><br>
+Anyone who has taken physics should know that higher frequency waves also have a higher energy, which can cause 
+adverse health effects like cancer. However, 5G still has a much lower frequency than the more dangerous 
+"ionising radiation", including high energy rays like ultraviolet and gamma. Due to its inability to ionise 
+particles (duh), it causes much less harm to us. Additionally, studies from the UK have found that 5G can only 
+penetrate the human body by 0.2cm, so the idea that 5G can cause cancer is but a myth.
+<br><br>
+5G causes plane crashes?
+<br><br>
+The frequency that flight instruments use to transmit data are typically in the range of 4.2GHz to 4.4GHz,
+much higher than the usual standard for 5G which is 3.3GHz to 3.8GHz (Quite ironically, since nobody says flight
+instruments are causing cancer), so the chance of interference is low. In fact, the USA has already permitted
+certain telecommunications companies to set up 5G networks in the vicinity of airports!
+<br><br>
+To summarise, 5G has little to no effects on humans or flight safety. You can enjoy 5G to the fullest without
+worrying about plane crashes or having your insides cooked. 
+`.replaceAll(`
+`," ")});g(w).mount("#app");
