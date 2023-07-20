eca55abfe2697f732a45b80cfebe2cf1b90e6888
qccomputerso <91327374+qccomputerso@users.noreply.github.com>
2023-07-20 15:37:19 +0000

    Deploying to gh-pages from @ qccomputerso/qccomputerso.github.io@eb93035170ca286930af82ffc7cfad44daaac26f 🚀

--- assets/main.ce6a9347.js
+++ assets/main.ce6a9347.js
@@ -1,18 +1,15 @@
-import{P as e,c as i,_ as o}from"./App.vue_vue_type_script_setup_true_lang.4ee3d21a.js";const t=""+new URL("committee-list.847f97b7.jpeg",import.meta.url).href;e.content.notification={title:"Programming Course",content:`
-In the following year, we will hold a programming course, with C++, a quick and versatile programming language,
-as our main language. You may even get a chance to learn web or game development! Fill in the google form now
-<a href="https://forms.gle/46ueCuvZb9rSSnAd6" target="_blank">HERE</a> to join. For any more information you may
-contact us- Details are at the bottom of the page.
-<br><br>
-Also, we have just published our FIRST EDITION of the publication "True or not True"!
-<a href="https://www.instagram.com/qc_socs/" target="_blank">Check our instagram page for more.</a>`};e.content.cards=[{headerImage:{href:t,width:400},title:"Welcome to our homepage!",content:`
+import{P as i,c as e,_ as t}from"./App.vue_vue_type_script_setup_true_lang.4ee3d21a.js";const o=""+new URL("committee-list.847f97b7.jpeg",import.meta.url).href;i.content.notification={title:"New school year",content:`
+We are continuously improving and aiming to make the school a better place for computer-lovers.
+<br>
+Stay tuned, and remember to <a href="https://www.instagram.com/qc_socs/" target="_blank">
+follow us on Instagram</a>!`};i.content.cards=[{headerImage:{href:o,width:400},title:"Welcome to our homepage!",content:`
 Our goal as the Society of Computer Studies is to provide insight about programming and Computer Science
 to our schoolmates, and to help them gain a better understanding of the topics, as well as an understanding
 of the impact of rapid development of IT on our society.
 <br>
 <br>
 We conduct
-<a href="https://qcsocs.com/program-course/" target="_blank">programming courses</a>,
+<a href="http://qcsocs.com/programming-course/" target="_blank">programming courses</a>,
 as well as training for the
 <a href="https://hkoi.org/en/" target="_blank">HKOI</a>
 (Hong Kong Olympiad in Informatics) to achieve our aims.
@@ -59,4 +56,4 @@ skills!`},{title:"Officials List",content:`
 	<button>
 		Contact Us >>
 	</button>
-</a>`}];i(o).mount("#app");
+</a>`}];e(t).mount("#app");
